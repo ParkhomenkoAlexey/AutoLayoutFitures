099efd478dcad24f6c74f436e721e9a398b1eb74
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2018-12-04 23:44:13 +0300

    Сделал 2 кубика через Visual Format Language.

--- Constraints App/ViewController.swift
+++ Constraints App/ViewController.swift
@@ -10,128 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let view1: UIView = {
+    let viewRed: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false // если не отключим то не даст работать с constraints
-        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
         view.backgroundColor = UIColor.red
         return view
     }()
     
-    let view2: UIView = {
+    let viewBlue: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false // если не отключим то не даст работать с constraints
-        view.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        view.backgroundColor = UIColor.green
+        view.backgroundColor = UIColor.blue
         return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(view1)
-        view.addSubview(view2)
-        //createView1Constraint()
-        createView1CenterConstraints()
-        createView2CenterConstraints()
+        view.addSubview(viewRed)
+        view.addSubview(viewBlue)
+        
+        // сделаем Visual Format Language
+        let viewVFL = ["viewRed":viewRed, "viewBlue":viewBlue]
+        // view.bounds.size.width / 3 - глобальную ширину делим на 3
+        // высота - view.bounds.size.height / 2 - берем высоты и режим на 2
+        let metrics = ["heingt":100, "widht": view.bounds.size.width / 3, "top": view.bounds.size.height / 2]
+        // "V" - вертикаль
+        // "H" - вертикаль
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewRed(heingt)]|",
+                                                          options: [],
+                                                          metrics: metrics,
+                                                          views: viewVFL))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-top-[viewBlue(heingt)]|",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
+        // если между [viewRed] и [viewblue] строит "-", то значит между ними constraint равен 8. Пример: "H:|[viewRed]-[viewBlue]|"
+        // "H:|[viewRed]-(50)-[viewBlue]|" - между ними 50
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[viewRed(widht)]-(50)-[viewBlue(widht)]-|",
+                                                           options: [],
+                                                           metrics: metrics,
+                                                           views: viewVFL))
         
     }
     
     
-    func createView1Constraint() {
-        NSLayoutConstraint(item: view1,
-                           attribute: .leadingMargin, // лево
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .leadingMargin,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .trailingMargin, // право
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .trailingMargin,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .top,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .topMargin,
-                           multiplier: 1,
-                           constant: 88).isActive = true // отступи 88 плз
-        NSLayoutConstraint(item: view1,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .width, // вниз
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        
-    }
     
-    // центруем
-    func createView1CenterConstraints() {
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .centerY,
-                           relatedBy: .equal,
-                           toItem: view,
-                           attribute: .centerY,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view1, // ширину говорим сами
-                           attribute: .width,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute, // поэтому и без атрибутов
-                           multiplier: 1,
-                           constant: 200).isActive = true
-        NSLayoutConstraint(item: view1,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 200).isActive = true
-    }
-    
-    func createView2CenterConstraints() {
-        NSLayoutConstraint(item: view2,
-                           attribute: .centerX,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .centerX,
-                           multiplier: 1,
-                           constant: 0).isActive = true
-        NSLayoutConstraint(item: view2,
-                           attribute: .bottom,
-                           relatedBy: .equal,
-                           toItem: view1,
-                           attribute: .top, // атрибут view1
-                           multiplier: 1,
-                           constant: -8).isActive = true
-        NSLayoutConstraint(item: view2, // ширину говорим сами
-            attribute: .width,
-            relatedBy: .equal,
-            toItem: nil,
-            attribute: .notAnAttribute, // поэтому и без атрибутов
-            multiplier: 1,
-            constant: 100).isActive = true
-        NSLayoutConstraint(item: view2,
-                           attribute: .height,
-                           relatedBy: .equal,
-                           toItem: nil,
-                           attribute: .notAnAttribute,
-                           multiplier: 1,
-                           constant: 100).isActive = true
-    }
 
 
 }
